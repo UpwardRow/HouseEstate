d6d05d6503d09939d8f7dda52cdd04d64b25044d
UpwardRow <downeyadam0312@gmail.com>
2023-04-16 23:47:45 +0100

    Added rubbish to houses

--- Houses.py
+++ Houses.py
@@ -3,25 +3,37 @@ import time
 
 class Houses:
 
-    def __init__(self, income, no_of_houses):
+    def __init__(self, income, no_of_houses, waste):
         self.income = income
         self.no_of_houses = no_of_houses
+        self.rubbish = waste
 
     def __repr__(self):
-        return f"income: {self.income}, no_of_houses: {self.no_of_houses}"
+        return f"income: {self.income}, no_of_houses: {self.no_of_houses}, waste: {self.rubbish}"
 
     def house_iteration(self):
         while True:
-            self.income += 1
 
-            time.sleep(1)
+            flag = False
 
-            if self.income % 10 == 0:
-                self.no_of_houses += 1
+            while self.rubbish != 35 and flag is not True:
 
-            print(f"The estate's income is: {self.income} and the number of houses is {self.no_of_houses}")
+                self.income += 1
+                self.rubbish += 0.5
+
+                print(f"The estate's income is: {self.income},"
+                      f" the number of houses is {self.no_of_houses},"
+                      f" and the waste buildup is {self.rubbish}")
+                time.sleep(1)
+                if self.income % 10 == 0:
+                    self.no_of_houses += 1
+                    flag = True
+
+            if self.rubbish == 35:
+                print("Rubbish overflow!")
+                time.sleep(2)
 
 
 if __name__ == "__main__":
-    house_object = Houses(3, 5)
+    house_object = Houses(3, 5, 30)
     house_object.house_iteration()
